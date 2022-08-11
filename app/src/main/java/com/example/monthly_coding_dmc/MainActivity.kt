4f25ba27ae0b8c22541293302378ef42e5a614e7
wogur2689 <81075170+wogur2689@users.noreply.github.com>
2022-08-12 02:27:47 +0900

    [refactor] restaurant_recommend detail

--- app/src/main/java/com/example/monthly_coding_dmc/MainActivity.kt
+++ app/src/main/java/com/example/monthly_coding_dmc/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.monthly_coding_dmc
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    //음식점 추천 액티비티로 이동
+    fun recommend(view: View) {
+        view.setOnClickListener {
+            val intent = Intent(this, RestaurantRecommendActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/example/monthly_coding_dmc/RestaurantRecommendActivity.kt
+++ app/src/main/java/com/example/monthly_coding_dmc/RestaurantRecommendActivity.kt
@@ -4,18 +4,34 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.monthly_coding_dmc.databinding.ActivityRestaurantRecommendBinding
 
+
 class RestaurantRecommendActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityRestaurantRecommendBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        binding = ActivityRestaurantRecommendBinding.inflate(layoutInflater)
+
         setContentView(binding.root) //바인딩
 
-        val fragmentList = listOf(FragmentA(), FragmentB(), FragmentC(), FragmentD()) //프래그먼트 목록 생성
+        val intent = intent //인텐트 받기
 
-        val adapter = FragmentAdapter(this) //adapter 생성
-        adapter.fragmentList = fragmentList  //앞에서 생선한 프래그먼트 목록을 저장.
-        binding.fragment1.adapter = adapter //레이아웃의 viewPager를 import하고 어댑터를 적용
+        //스피드
+        val arraySpeed1 = resources.getStringArray(R.array.restaurant_array33) //리소스에서 string 가져오기
+        binding.restaurantTextSpeed1.text = arraySpeed1[0] //서브웨이
+        val arraySpeed2 = resources.getStringArray(R.array.restaurant_array33) //리소스에서 string 가져오기
+        binding.restaurantTextSpeed2.text = arraySpeed2[0] //버거킹
+        val arraySpeed3 = resources.getStringArray(R.array.restaurant_array33) //리소스에서 string 가져오기
+        binding.restaurantTextSpeed3.text = arraySpeed3[0] //KFC
+
+        //매운맛
+        val array = resources.getStringArray(R.array.restaurant_array33) //리소스에서 string 가져오기
+        binding.restaurantTextSpeed1.text = array[0]
+
+        //맛집
+
+        //달콤한맛
     }
 }
