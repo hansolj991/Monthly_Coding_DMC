a5c2d188792f57d22c6191f59df8a488ab4a0411
wogur2689 <81075170+wogur2689@users.noreply.github.com>
2022-08-12 12:51:23 +0900

    [refactor] string array index change

--- app/src/main/java/com/example/monthly_coding_dmc/RestaurantRecommendActivity.kt
+++ app/src/main/java/com/example/monthly_coding_dmc/RestaurantRecommendActivity.kt
@@ -19,35 +19,35 @@ class RestaurantRecommendActivity : AppCompatActivity() {
         val intent = intent //인텐트 받기
 
         //스피드
-        val arraySpeed1 = resources.getStringArray(R.array.restaurant_array33) //리소스에서 string 가져오기
-        binding.restaurantTextSpeed1.text = arraySpeed1[0]
+        val arraySpeed1 = resources.getStringArray(R.array.restaurant_array5) //리소스에서 string 가져오기
+        binding.restaurantTextSpeed1.text = arraySpeed1[1] //서브웨이
         val arraySpeed2 = resources.getStringArray(R.array.restaurant_array33)
-        binding.restaurantTextSpeed2.text = arraySpeed2[0]
+        binding.restaurantTextSpeed2.text = arraySpeed2[1]
         val arraySpeed3 = resources.getStringArray(R.array.restaurant_array33)
-        binding.restaurantTextSpeed3.text = arraySpeed3[0]
+        binding.restaurantTextSpeed3.text = arraySpeed3[1]
 
         //매운맛
         val arraySpicy1 = resources.getStringArray(R.array.restaurant_array33)
-        binding.restaurantTextSpicy1.text = arraySpicy1[0] //죠스 떡볶이
-        val arraySpicy2 = resources.getStringArray(R.array.restaurant_array33)
-        binding.restaurantTextSpicy2.text = arraySpicy2[0]
-        val arraySpicy3 = resources.getStringArray(R.array.restaurant_array33)
-        binding.restaurantTextSpicy3.text = arraySpicy3[0]
+        binding.restaurantTextSpicy1.text = arraySpicy1[1] //죠스 떡볶이
+        val arraySpicy2 = resources.getStringArray(R.array.restaurant_array50)
+        binding.restaurantTextSpicy2.text = arraySpicy2[1] //하우 마라
+        val arraySpicy3 = resources.getStringArray(R.array.restaurant_array34)
+        binding.restaurantTextSpicy3.text = arraySpicy3[1] //엽기 떡볶이
 
         //맛집
         val arrayRecommend1 = resources.getStringArray(R.array.restaurant_array33)
-        binding.restaurantTextRecommend1.text = arrayRecommend1[0] //닥터로빈
+        binding.restaurantTextRecommend1.text = arrayRecommend1[1]
         val arrayRecommend2 = resources.getStringArray(R.array.restaurant_array3)
-        binding.restaurantTextRecommend2.text = arrayRecommend2[0] //동명
+        binding.restaurantTextRecommend2.text = arrayRecommend2[1] //동명
         val arrayRecommend3 = resources.getStringArray(R.array.restaurant_array8)
-        binding.restaurantTextRecommend3.text = arrayRecommend3[0] //지지고
+        binding.restaurantTextRecommend3.text = arrayRecommend3[1] //지지고
 
         //달콤한맛
         val arraySweet1 = resources.getStringArray(R.array.restaurant_array33)
-        binding.restaurantTextSweet1.text = arraySweet1[0]
+        binding.restaurantTextSweet1.text = arraySweet1[1]
         val arraySweet2 = resources.getStringArray(R.array.restaurant_array33)
-        binding.restaurantTextSweet2.text = arraySweet2[0]
+        binding.restaurantTextSweet2.text = arraySweet2[1]
         val arraySweet3 = resources.getStringArray(R.array.restaurant_array33)
-        binding.restaurantTextSweet3.text = arraySweet3[0]
+        binding.restaurantTextSweet3.text = arraySweet3[1]
     }
 }
